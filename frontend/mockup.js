a37b1f258a16bebe26748f1378a66d3cbe22c295
Bereket Semagn <semagnbereket@gmail.com>
2021-01-27 15:24:19 -0500

    frontend build almost complete

---
+++ frontend/mockup.js
@@ -0,0 +1,34 @@
+const section1 = document.getElementById("section1");
+const section2 = document.getElementById("section2");
+const section3 = document.getElementById("section3");
+const resultImg = document.getElementById("resultImg");
+const Download = document.getElementById("Download");
+
+const submitBtn = document.getElementById("submitBtn");
+
+
+const generateMockup = () => {
+  const websiteAdress = document.getElementById("websiteAdress").value;
+  const bgColor = document.getElementById("bgColor").value;
+  const mockup =
+    "https://2s9e3bif52.execute-api.eu-central-1.amazonaws.com/production/screenshot?url=" +
+    websiteAdress +
+    "&color=" +
+    bgColor;
+  console.log(mockup);
+  resultImg.src = mockup;
+
+  section1.style.display = "none";
+  section2.style.display = "block";
+
+  Download.addEventListener("click", function () {
+    window.open(mockup);
+  });
+};
+
+const changeView = () => {
+  setTimeout(() => {
+    section2.style.display = "none";
+    section3.style.display = "block";
+  }, 6500);
+};
